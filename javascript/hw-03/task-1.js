15f15306079f514011cd36eb9a8904c09d7c79f6
eyesdwn <eyesdwn@gmail.com>
2019-09-24 09:12:27 +0300

    hw-03

---
+++ javascript/hw-03/task-1.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const user = {
+    name: 'Mango',
+    age: 20,
+    hobby: 'html',
+    premium: true,
+};
+
+user.mood = 'happy';
+user.hobby = 'javascript';
+user.premium = false;
+
+
+for (const key in user) {
+    console.log(`${key}: ${user[key]}`);
+};
