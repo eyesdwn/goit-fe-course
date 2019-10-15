e5a82789337dff31668701a963a4a7fd70d19560
Tatiana <eyesdwn@gmail.com>
2019-10-15 20:03:41 +0300

    hw-04 updated

--- javascript/hw-04/task-2.js
+++ javascript/hw-04/task-2.js
@@ -1,24 +1,26 @@
 const inventory = {
-    items: ['Монорельса', 'Фильтр'],
-    add(itemName) {
-      this.items.push(itemName);
-    },
-    remove(itemName) {
-      this.items = this.items.filter(item => item !== itemName);
-    },
-  };
-  
-  const invokeInventoryAction = function(inventory, itemName, action) {
-    console.log(`Invoking ${action.name} opeartion on ${itemName}`);
-    action.call(inventory, itemName);
-  };
-  
-  invokeInventoryAction(inventory, 'Аптечка', inventory.add);
-  // Invoking add opeartion on Аптечка
-  
-  console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
-  
-  invokeInventoryAction(inventory, 'Фильтр', inventory.remove);
-  // Invoking remove opeartion on Фильтр
-  
-  console.log(inventory.items); // ['Монорельса', 'Аптечка']
+  items: ['Монорельса', 'Фильтр'],
+  add(itemName) {
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryOperation = function (itemName, inventoryAction) {
+  console.log(`Invoking ${inventoryAction.name} opeartion on ${itemName}`);
+  inventoryAction.call(this, itemName);
+};
+const boundInventoryAdd = inventory.add.bind(inventory);
+const boundInventoryRemove = inventory.remove.bind(inventory);
+
+invokeInventoryOperation('Аптечка', boundInventoryAdd);
+// Invoking add opeartion on Аптечка
+
+console.log(inventory.items); // ['Монорельса', 'Фильтр', 'Аптечка']
+
+invokeInventoryOperation('Фильтр', boundInventoryRemove);
+// Invoking remove opeartion on Фильтр
+
+console.log(inventory.items); // ['Монорельса', 'Аптечка']
