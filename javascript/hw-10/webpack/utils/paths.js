3f8dc6b0f16864f1d494920bb92dcddde0cba3e7
Tatiana <eyesdwn@gmail.com>
2019-11-16 11:12:36 +0200

    build

--- javascript/hw-10/webpack/utils/paths.js
+++ javascript/hw-10/webpack/utils/paths.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const paths = {
   SRC_DIR: path.resolve(__dirname, '../../src'),
-  BUILD_DIR: path.resolve(__dirname, '../../build'),
+  BUILD_DIR: path.resolve(__dirname, '../../dist'),
 };
 
 module.exports = paths;
