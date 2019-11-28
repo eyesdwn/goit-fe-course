4a6241e8f453df5e306836a618fe5703b8035e50
Tatiana <eyesdwn@gmail.com>
2019-11-28 17:36:27 +0200

    hw-11-timer

---
+++ javascript/hw-11-timer/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const refs = {
+  days: document.querySelector('span[data-value="days"'),
+  hours: document.querySelector('span[data-value="hours"'),
+  mins: document.querySelector('span[data-value="mins"'),
+  secs: document.querySelector('span[data-value="secs"'),
+};
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+  updateClockface(days, hours, mins, secs) {
+    refs.days.textContent = days;
+    refs.hours.textContent = hours;
+    refs.mins.textContent = mins;
+    refs.secs.textContent = secs;
+  }
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  start() {
+    this.timerId = setInterval(() => {
+      const currentTime = Date.now();
+      const time = this.targetDate - currentTime;
+      const date = new Date(time);
+
+      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+      const hours = this.pad(
+        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+      const mins = this.pad(
+        Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+      );
+      const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+      this.updateClockface(days, hours, mins, secs);
+    }, 1000);
+  }
+}
+
+const countdownTimer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Dec 31, 2019'),
+});
+
+countdownTimer.start();
