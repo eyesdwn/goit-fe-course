d7a0f7fa55f40b951b10a1f69ee33ff6ac9df7c9
Tatiana <eyesdwn@gmail.com>
2019-12-02 16:09:51 +0200

    updated

--- javascript/hw-11-color-switch/index.js
+++ javascript/hw-11-color-switch/index.js
@@ -13,10 +13,11 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-function setNewColor() {
-  let newColor = randomIntegerFromInterval(0, 6);
+function setNewColor(arr) {
+  let newColor = randomIntegerFromInterval(0, arr.length);
   document.body.style.backgroundColor = colors[newColor];
 }
+
 let colorInterval;
 let isActive = false;
 
@@ -24,7 +25,7 @@ function changeBackgroundColor(e) {
   // console.log(e.target);
 
   if (e.target.dataset.action === 'start' && isActive === false) {
-    colorInterval = setInterval(() => setNewColor(), 1000);
+    colorInterval = setInterval(() => setNewColor(colors), 1000);
     isActive = true;
   } else if (e.target.dataset.action === 'stop') {
     clearInterval(colorInterval);
